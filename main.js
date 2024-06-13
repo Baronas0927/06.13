2a34677f1d1b6b6adeccc23074b4848f61a24b37
Baronas0927 <do.baublys@gmail.com>
2024-06-13 09:44:21 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+//oru API
+console.log("jo");
+
+document.querySelector("#submit").addEventListener("click", searchCity)
+fetch("https://api.meteo.lt/v1/places/adakavas/forecasts/long-term")
+    .then(response=> {return response.json()})
+    .then(data => {printToConsole(data); })
+
+function printToConsole(data){
+    console.log(data["place"]);//istraukia vietos pavadinima
+    console.log(data.place);//istraukia vietos pavadinima
+    console.log(data.place.name);
+}
+
+function searchCity(e){
+    e.preventDefault();
+    let city = document.querySelector("#city");
+    console.log(city.value);
+    city.value = "";
+
+}
